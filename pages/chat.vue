970e1ea5a9d447728459a974e9a5e8c17f807c9a
dima- <dima-tkachenko@mail.ru>
2021-02-25 00:28:41 +0300

    04

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>CHAT PAGE {{ user.name }}</h1>
+  <div>
+    <ul>
+      <li v-for="m in messages" :key="m.text">{{ m.text }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -12,6 +16,6 @@ export default {
       title: `Комната ${this.user.room}`
     };
   },
-  computed: mapState(["user"])
+  computed: mapState(["user", "messages"])
 }
 </script>
